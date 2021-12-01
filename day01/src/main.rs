09dd18cd2c08073a0fd0f33255a9de6696e52dfd
kebabtent <kebabtent@gmail.com>
2021-12-01 23:32:20 +0100

    Day 01

---
+++ day01/src/main.rs
@@ -0,0 +1,14 @@
+use common::{read_lines_as, DoubletSum};
+use itertools::Itertools;
+
+fn main() {
+	let (a, b) = read_lines_as::<u32>("01")
+		.tuple_windows()
+		.enumerate()
+		.map(|(i, (a, b, c, d))| (i < 2 && b > a, d > c, b + c + d > a + b + c))
+		.map(|(a, b, c)| (a as u32 + b as u32, c))
+		.doublet_sum::<u32, u32>();
+
+	println!("{}", a);
+	println!("{}", b);
+}
