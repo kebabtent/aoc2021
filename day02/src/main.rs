e77d9e77803efae9090d0a8cc4f07bdae1fc36fb
kebabtent <kebabtent@gmail.com>
2021-12-02 13:12:59 +0100

    Day 02

---
+++ day02/src/main.rs
@@ -0,0 +1,17 @@
+use common::{read_lines, IterExt};
+
+fn main() {
+	let (x, y, z) = read_lines().fold((0, 0, 0), |(x, y, z), l| {
+		let (c, v) = l.split(" ").next_doublet().unwrap();
+		let v = v.parse::<u32>().unwrap();
+		match c {
+			"forward" => (x + v, y, z + y * v),
+			"down" => (x, y + v, z),
+			"up" => (x, y - v, z),
+			_ => unreachable!(),
+		}
+	});
+
+	println!("{}", x * y);
+	println!("{}", x * z);
+}
