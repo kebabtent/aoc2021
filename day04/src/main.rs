20e90d6c9d3624d7e275255c15fda53b38c29c23
kebabtent <kebabtent@gmail.com>
2021-12-19 15:28:41 +0100

    Day 04: replace u8 vector with u64

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,53 +1,64 @@
 use common::{read_all_lines, IterExt};
-use retain_mut::RetainMut;
 
 fn main() {
 	let mut t = read_all_lines();
 	let n = t.next().unwrap();
-	let mut b = t
+	let mut d = t
 		.batching(|t| {
 			t.next()?;
-			let mut s = vec![vec![0u8; 5]; 10];
+			let mut s = [0u64; 11];
 			for i in 0..5 {
 				let l = t.next()?;
-				let mut w = l.split(" ").filter(|&w| !w.is_empty());
-				for j in 0..5 {
-					let v = w.next()?.parse().ok()?;
-					s[i][j] = v;
-					s[j + 5][i] = v;
+				let w = l
+					.split(" ")
+					.filter(|&w| !w.is_empty())
+					.filter_map(|w| w.parse::<u64>().ok())
+					.enumerate();
+				for (j, v) in w {
+					s[i + 1] += v + 1 << 8 * j;
+					s[j + 6] += v + 1 << 8 * i;
 				}
 			}
 			Some(s)
 		})
 		.collect::<Vec<_>>();
-	let m = b.len();
+	let m = d.len();
 
 	let (a, b) = n
 		.split(",")
-		.filter_map(|v| v.parse::<u8>().ok())
-		.fold_while((0usize, 0u32, 0u32), |(c, a, d), v| {
-			b.retain_mut(|s| {
+		.filter_map(|v| v.parse::<u64>().ok())
+		.map(|v| v + 1)
+		.fold_while((0, 0, 0), |(c, a, b), v| {
+			for s in d.iter_mut().filter(|s| s[0] == 0) {
 				let mut o = false;
-				for r in s.iter_mut() {
-					r.retain(|&x| x != v);
-					if r.len() == 0 {
+				for r in s.iter_mut().skip(1) {
+					for i in 0..5 {
+						if (*r >> 8 * i) & 0xFF == v {
+							*r -= v << 8 * i;
+						}
+					}
+					if *r == 0 {
 						o = true;
 					}
 				}
 				if o {
+					s[0] = 1;
 					*c += 1;
-					if *c == 1 {
-						*a = u(s) * v as u32;
-					}
-					if *c == m {
-						*d = u(s) * v as u32;
+					if *c == 1 || *c == m {
+						let u = s.iter().skip(1).take(5).fold(0, |a, &r| {
+							a + (0..5).fold(0, |a, i| a + ((r >> 8 * i) & 0xFF).saturating_sub(1))
+						}) * (v - 1);
+						if *c == 1 {
+							*a = u;
+						} else {
+							*b = u;
+						}
 					}
 				}
-				!o
-			});
+			}
 
-			if b.len() == 0 {
-				Some((*a, *d))
+			if *c == m {
+				Some((*a, *b))
 			} else {
 				None
 			}
@@ -56,10 +67,4 @@ fn main() {
 
 	println!("{}", a);
 	println!("{}", b);
-}
-
-fn u(s: &[Vec<u8>]) -> u32 {
-	s.iter()
-		.take(5)
-		.fold(0, |a, r| a + r.iter().map(|&v| v as u32).sum::<u32>())
 }
